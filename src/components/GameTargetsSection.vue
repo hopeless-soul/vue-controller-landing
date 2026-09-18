c117ed592f030e55da4ecac82ac325177896f062
hopeless-soul <r3al.hum4n@gamil.com>
2026-09-18 22:27:51 +0200

    feat: add GameTargetsSection component

---
+++ src/components/GameTargetsSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import bokiagepTarget from '@/assets/bokiagep-target.png'
+
+const TARGET_COUNT = 5
+</script>
+
+<template>
+  <section class="flex items-start justify-center gap-[98px] pt-[120px]">
+    <img
+      v-for="n in TARGET_COUNT"
+      :key="n"
+      :src="bokiagepTarget"
+      alt=""
+      class="h-[56px] w-[56px]"
+      aria-hidden="true"
+    />
+  </section>
+</template>
